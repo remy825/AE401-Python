94e946b7f3d2e62294d3b483f156a7704e6d7f5d
remy825 <65432769+remy825@users.noreply.github.com>
2020-05-16 11:00:58 +0800

    Unit1
    raidus

---
+++ raidus_remy_0516.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 16 10:39:11 2020
+
+@author: remy
+"""
+
+print("enter you radius")
+r = input("your circus radius is :")
+r = float(r)
+o='%2.2f'%(r*2*3.14)
+a=r*r*3.14
+
+print('perimeter is',o)
+print('area is', a)
