3d4e521ad37cffc993869b3b0a061080cc89a303
remy825 <65432769+remy825@users.noreply.github.com>
2020-05-16 12:07:04 +0800

    unit2

---
+++ ji3g418 fu.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 16 11:00:04 2020
+
+@author: remy
+"""
+
+
+score = input('input')
+score = int (score)
+
+if (score > 70):
+    print("pass")
+elif(score ==70):
+    print("go keeping")
+else:
+    print("fail")
