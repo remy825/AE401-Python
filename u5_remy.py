328a87717857566846a29570ecd887c118b25f1f
remy825 <65432769+remy825@users.noreply.github.com>
2020-06-06 12:00:59 +0800

    Add files via upload

---
+++ u5_remy.py
@@ -0,0 +1,25 @@
+scores = []
+sum = 0
+highest = 0
+lowest = 100
+
+n = input('people?')
+n = int(n)
+
+for i in range(x):
+     score = input("input score")
+     score = int(score)
+     scores.append(scores)
+     
+for i in range(n):
+    if scores[i] > highest:
+        highest = scores[i]
+    if scores[i] < lowest:
+        lowest = scores[i
+    sum = scores[i] + sum
+average = sum / n
+
+print("average:",average)
+print("highest:",highest)
+print("lowest:",lowest)
+     
