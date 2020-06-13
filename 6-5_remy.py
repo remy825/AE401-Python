da1d8450a69bc36e294e80931c3e479d482d089a
remy825 <65432769+remy825@users.noreply.github.com>
2020-06-13 12:00:19 +0800

    Add files via upload

---
+++ 6-5_remy.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 13 11:38:26 2020
+
+@author: remy
+"""
+
+def add3(a, b, c):
+    answer = a+b+c
+    return answer
+
+a1 = add3(1,2,3)
+a2 = add3(3,6,9)
+a3 = add3(12,45,78)
+print(a1, a2, a3)
