1d50b754d28fc0521d219bd381de928fa2378101
remy825 <65432769+remy825@users.noreply.github.com>
2020-08-15 11:25:55 +0800

    Add files via upload

---
+++ multiplicationtable_remy.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 15 11:07:57 2020
+
+@author: remy
+"""
+
+import random
+for i in range(1,10):
+    for j in range(1,10):
+        print(i,"x",j,"=",i*j)
+
